0befd4438cb6cc2ff4051ee1c7fea2d5778fef60
raaraan <henna@stanford.edu>
2014-03-05 06:37:04 +0000

    done for now

--- public/js/pick.js
+++ public/js/pick.js
@@ -6,36 +6,49 @@ $(document).ready(function() {
 	$('button.redirect-movie').click(changeImage);
 })
 
+var images = [
+		"/images/racoon.png",
+		"/images/sas.png"
+	];
+
+	var randInt=1;
+
 
 function changeImage(e){
 	e.preventDefault();
 	console.log("hello");
-	var randInt = Math.floor(Math.random() * (6-1+1)) + 1;
+	//var randInt = Math.floor(Math.random() * (6-1+1)) + 1;
 	console.log(randInt);
 	var image;
 	var title;
 	switch(randInt){
 		case 1:
+			randInt = 2;
 			image = "/images/racoon.png";
 			title = "Remus the Raccoon";
 			break;
-		case 2:
-			image = "/images/sas.png";
-			title = "Sasquatch";
+		case 6:
+		randInt = 1;
+			image = "/images/leonardo.png";
+			title = "Deranged da Vinci";
 			break;
 		case 3:
+		randInt = 4;
 			image = "/images/rat.png";
 			title = "Emile the rat";
 			break;
 		case 4:
+		randInt = 5;
 			image = "/images/man.png";
 			title = "Bob the businessman";
 			break;
 		case 5:
+		randInt = 6;
 			image = "/images/pirate.png";
 			title = "Pete the pirate";
 			break;
-		case 6:
+		case 2:
+		randInt = 3;
 			image = "/images/madsci.png";
 			title = "McMillius the mad scientist";
 			break;
